dcd956f1049c1c4f25f56450d559fbf9554104e1
SpamixOfficial <spamixproducer@gmail.com>
2025-03-19 08:53:46 +0100

    Add test files, writeup tool updated, spec in README now

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # Giffel writeups
 
 Very serious writeups :neckbeard:
+
+## Markdown Metadata Tag
+
+To display metadata on our writeup site we include a metadata tag in every markdown writeup.
+
+**NOTE: THIS TAG IS MANDATORY**
+
+The tag is as following
+
+```markdown
+<!--BKFG
+title="Fake"
+author="TheGoat"
+date="2024/03/07"
+-->
+```
+
+It is a modified html comment and should be included in the start of every writeup.
+
+The data included is in the **TOML** format because it's easy to use.
+
+***For metadata fields, please consult the table below***
+
+### Metadata fields
+
+**NOTE: Since I (spamix) am a rust stan I've just used _rust-like_ types below. _VERY easy_ to understand tbh**
+
+| name   | description                  | optional | type        |
+| ------ | ---------------------------- | -------- | ----------- |
+| title  | Title of the writeup         | false    | String      |
+| author | Author of the writeup        | true     | String      |
+| date   | Date the writeup was written | true     | String/Date |
+
+More fields to come!
--- writeup.py
+++ writeup.py
@@ -16,20 +16,23 @@ def update_manifest():
     if not os.path.exists(os.path.join(directory,"manifest.json")):
         print(f"[ERROR] There's no manifest.json present here ({directory}), have you specified a correct directory?")
         exit(1)
-    json.dump(fp=open(os.path.join(directory, "manifest.json"), "w"), obj=manifest)
+    json.dump(fp=open(os.path.join(directory, "manifest.json"), "w"), ensure_ascii=False, obj=manifest)
 
-def create_ctf(name, categories, yearmonth):
+def create_ctf(name, categories, yearmonth, dirname=None):
     global directory, manifest
-    
+    if dirname == None:
+        dirname = re.sub(r'[^\x00-\x7F]', '_', name)
+    else:
+        dirname = dirname[0]
+
     categories = [c.lower() for c in categories] # Lowercase em all
-    legalname = re.sub(r'[^\x00-\x7F]', '_', name) # Make name legal 
-    newbasepath = os.path.join(directory, legalname)
+    newbasepath = os.path.join(directory, dirname)
     if os.path.exists(newbasepath):
         print(f"[ERROR] Directory {name} already exists!!")
         exit(1)
 
     os.makedirs(newbasepath)
-    manifest["competitions"].append(name)
+    manifest["competitions"].append(dirname)
 
     update_manifest()
 
@@ -43,7 +46,7 @@ def create_ctf(name, categories, yearmonth):
     for c in categories:
         os.makedirs(os.path.join(newbasepath, c))
     
-    json.dump(fp=open(os.path.join(newbasepath, "manifest.json"), "w"), obj=ctf_manifest)
+    json.dump(fp=open(os.path.join(newbasepath, "manifest.json"), "w"), obj=ctf_manifest, ensure_ascii=False)
 
 
 parser = argparse.ArgumentParser(
@@ -58,6 +61,7 @@ subparsers = parser.add_subparsers(help='Commands')
 p_create = subparsers.add_parser('create', help="Create new ctf")
 p_create.set_defaults(which="p_create") # Identifier
 p_create.add_argument("name", nargs=1, help="Ctf name")
+p_create.add_argument("-n", "--dirname", nargs=1, required=False, help="A legal directory name, in case you want the name to be something else than the directory name use this option!", default=None)
 p_create.add_argument("-c", "--categories", nargs="+", required=True, help="Categories")
 p_create.add_argument('-t', '--yearmonth', nargs=1, required=True, help="When it happened")
 
@@ -68,4 +72,4 @@ get_manifest()
 
 match args.which:
     case "p_create":
-        create_ctf(args.name[0], args.categories, args.yearmonth[0])
+        create_ctf(args.name[0], args.categories, args.yearmonth[0], args.dirname)
